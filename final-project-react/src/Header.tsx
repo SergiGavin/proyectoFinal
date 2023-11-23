37d642a0a65d4d595aa84a3ff142051ea0d5a2e2
Mariangelsmd <mariangelesmolina1995@gmail.com>
2023-11-23 19:09:27 +0100

    Update 23nov

--- final-project-react/src/Header.tsx
+++ final-project-react/src/Header.tsx
@@ -2,20 +2,24 @@ import "./Header.css"
 
 function Header() {
     return (
-    <>
-        <div className="row fndo-naranja">
-            <div className="col-4">
-                <div className="logo"><p>SwapReads</p></div>
-            </div>
-            <div className="col-6">
-                <div className="buscador"></div>
-            </div>
-            <div className="col">
-                <button type="button" className="btn btn-inicioSesion btn-hover">Inicio sesión</button>
-            </div>
+        <>
+                <div className="row fndo-naranja">
+                    <div className="col-3">
+                        <p className="nombre mt-3">SwapReads</p>
+                    </div>
+                    <div className="col-6">
+                    <div className="input-group mb-3 mt-3">
+                    <span className="input-group-text" id="inputGroup-sizing-default">Buscar</span>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default"/>
+                    </div>
+                    </div>
+                    <div className="col-3">
+                        <button type="button" className="btn btn-inicioSesion btn-hover">Inicio sesión</button>
+                    </div>
 
-        </div>
-    </>
+                </div>
+
+        </>
     )
 }
 
