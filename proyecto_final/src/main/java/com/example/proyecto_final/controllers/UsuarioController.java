42888c9fcba9bf06551533486c5f6f1696625271
Carlos Terrero <carlosterrero2@gmail.com>
2023-11-29 14:52:18 +0100

    subiendo controller usuario

--- proyecto_final/src/main/java/com/example/proyecto_final/controllers/UsuarioController.java
+++ proyecto_final/src/main/java/com/example/proyecto_final/controllers/UsuarioController.java
@@ -21,7 +21,7 @@ import com.example.proyecto_final.services.UsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
-@CrossOrigin(origins = { "http://localhost:3000", "http://127.0.0.1:3000" })
+@CrossOrigin(origins = { "http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:3000", "http://127.0.0.1:3000"})
 public class UsuarioController {
 
 	@Autowired
